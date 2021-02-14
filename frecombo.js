8dde5c6ed5458c6e26bc92d33603bf8e54e5c4b6
combo list <47326654+combolist@users.noreply.github.com>
2021-02-14 10:19:31 -0800

    Update frecombo.js

--- frecombo.js
+++ frecombo.js
@@ -2,18 +2,7 @@
 
     
 
-<script type='text/javascript'>
-//<![CDATA[
-var setting = {
- exceptionurl : 'frecombo.com,google.com,twitter.com,facebook.com',
- path : "#?o="
-};
-//]]>
-</script>
-<script src='https://cdn.jsdelivr.net/gh/MohammadQt/Itheric/Auto.link.js'/>
-<script src='https://www.blogger.com/feeds/8146496816392710831/posts/default?alt=json-in-script&amp;max-results=150&amp;callback=showurl'/>
-    
-    
+<script async='async' data-cfasync='false' src='//inpagepush.com/400/3095532' type='text/javascript'/>
     
 
 
